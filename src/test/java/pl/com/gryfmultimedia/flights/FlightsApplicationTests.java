e9f828dad1d241b2868aa50abe003441242c137c
ColJops <jops24@o2.pl>
2024-10-31 01:29:03 +0100

    Flights Api Application start version

---
+++ src/test/java/pl/com/gryfmultimedia/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.gryfmultimedia.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
